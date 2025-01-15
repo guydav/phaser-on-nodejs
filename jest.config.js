fb30db133e3f90f13a993091027677d7f400b9cc
Guy Davidson <guydav@gmail.com>
2025-01-15 00:45:36 -0500

    Got the fucking tests to fucking pass

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,11 @@ module.exports = {
   maxWorkers: 1,
   testTimeout: 10_000,
   testEnvironment: 'jsdom',
-  setupFiles: ['./jest.polyfills.js'],
+  // Adding lib/index.js here so it doesn't get preprocessed and transformed
+  setupFiles: ['./jest.setup.js', '<rootDir>/lib/index.js'],
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/lib/index.js"],
+  // To get it to load the CJS, rathre than ES module:
+  moduleNameMapper: {
+    "@asamuzakjp/css-color": "<rootDir>/node_modules/@asamuzakjp/css-color/dist/cjs/index.cjs"
+  }
 }
---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+
+require("@inrupt/jest-jsdom-polyfills");
+
+// const config = {
+// //   setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
+// };
+
+// export default config;
+
